cea718104e5d46c68065c2aff5fbac20631c64cc
derossca <derossca@mail.uc.edu>
2024-03-29 15:58:08 -0400

    making private methods for the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-//declaring board array and constants that define it
-
+//importing scanner
+import java.util.Scanner;
 
 public class Main {
     //declaring board array and the constants that define it
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args)
     {
         //pseudo code
+
 
     }
 
@@ -38,39 +39,63 @@ public class Main {
         }
     }
 
-    /*returns true if there is a space at the given proposed move coordinates which means it is a legal move
+    //returns true if there is a space at the given proposed move coordinates which means it is a legal move
     private static boolean isValidMove(int row, int col)
     {
-
+        return board[row][col].equals(" "); //only returns true if the cell is a space
     }
 
-    checking to see if there is a win state for the specified player (X or O)
+    //checking to see if there is a win state for the specified player (X or O)
     private static boolean isWin(String player)
     {
         //checking for a col win
         private static boolean isColWin(String player)
         {
-
+            for(int col = 0; col < COL; col++)
+            {
+                if(board[0][col].equals(player) && board[1][col].equals(player) && board[2][col].equals(player))
+                {
+                    return true;
+                }
+            }
+            return false; // no col win
         }
 
         //checking for a row win
         private static boolean isRowWin(String player)
         {
-
+            for(int row = 0; row < ROW; row++)
+            {
+                if(board[row][0].equals(player) && board[row][1].equals(player) && board[row][2].equals(player))
+                {
+                    return true;
+                }
+            }
+            return false; // no row win
         }
 
         //checks for a diagonal win
         private static boolean isDiagonalWin(String player)
         {
-
+            if((board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player)) || (board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player)))
+            {
+                return true;
+            }
+            return false; // no diagonal win
         }
 
-        //checks for a tie
+        /*checks for a tie
         private static boolean isTie()
         {
 
         }
-    }
 
-     */
+         */
+
+        if(isColWin(player) || isRowWin(player) || isDiagonalWin(player))
+        {
+            return true;
+        }
+        return false;
+    }
 }
