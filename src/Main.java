ec0b3cc6d386d8e397f658344e0d0323613eb18b
derossca <derossca@mail.uc.edu>
2024-03-29 14:49:20 -0400

    adjusting methods

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,45 @@ public class Main {
     public static void main(String[] args)
     {
 
+        //testing
+        clearBoard();
+        display();
+
     }
 
     //Helper methods for the TicTacToe game
     private static void clearBoard() //setting all board elements to a space
     {
-
+        for(int row = 0; row < ROW; row++)
+        {
+            for(int col = 0; col < COL; col++)
+            {
+                board[row][col] = " "; //makes this cell a space
+            }
+        }
     }
 
     //shows the Tic Tac Toe game used as part of the prompt for the users move choice
     private static void display()
     {
-
+        for(int row = 0; row < ROW; row++)
+        {
+            for(int col = 0; col < COL; col++)
+            {
+                board[row][col] = " _|_ ";
+                System.out.print(board);
+            }
+            System.out.println();
+        }
     }
 
-    //returns true if there is a space at the given proposed move coordinates which means it is a legal move
+    /*returns true if there is a space at the given proposed move coordinates which means it is a legal move
     private static boolean isValidMove(int row, int col)
     {
 
     }
 
-    //checking to see if there is a win state for the specified player (X or O)
+    checking to see if there is a win state for the specified player (X or O)
     private static boolean isWin(String player)
     {
         //checking for a col win
@@ -56,4 +74,6 @@ public class Main {
 
         }
     }
+
+     */
 }
