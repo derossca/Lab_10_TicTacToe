a6e2cba436de37417e1a6065c3ba11fb3638687c
derossca <derossca@mail.uc.edu>
2024-03-29 14:26:57 -0400

    setting up game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+//declaring board array and constants that define it
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //declaring board array and the constants that define it
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static String board[][] = new String[ROW][COL];
+    public static void main(String[] args)
+    {
+
+    }
+
+    //Helper methods for the TicTacToe game
+    private static void clearBoard() //setting all board elements to a space
+    {
+
+    }
+
+    //shows the Tic Tac Toe game used as part of the prompt for the users move choice
+    private static void display()
+    {
+
+    }
+
+    //returns true if there is a space at the given proposed move coordinates which means it is a legal move
+    private static boolean isValidMove(int row, int col)
+    {
+
+    }
+
+    //checking to see if there is a win state for the specified player (X or O)
+    private static boolean isWin(String player)
+    {
+        //checking for a col win
+        private static boolean isColWin(String player)
+        {
+
+        }
+
+        //checking for a row win
+        private static boolean isRowWin(String player)
+        {
+
+        }
+
+        //checks for a diagonal win
+        private static boolean isDiagonalWin(String player)
+        {
+
+        }
+
+        //checks for a tie
+        private static boolean isTie()
+        {
+
+        }
     }
 }
